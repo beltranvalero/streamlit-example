57b1dbbb00cefd9a29631326d8e50072fad2bcf3
beltranvalero <112970415+beltranvalero@users.noreply.github.com>
2023-03-30 09:51:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ Our goal will be show how diffferent polutants in the air affect asthma rate wit
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 data = pd.read_csv('Cali2018Form.csv')
 print(data)
